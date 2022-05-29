327ade73b20eb6e1c3d1cdead21a902c888fc986
Deva-Mari <devamari@gmx.net>
2022-05-30 10:17:03 +1200

    added html and json data incl clickHandlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import data from "./data/data.json";
+import styles from "./css/Slider.module.css";
 
 function App() {
+  const [image, setImage] = useState("");
+  const [person, setPerson] = useState(0);
+  import(`./assets/${data[person].img}`).then((image) =>
+    setImage(image.default)
+  );
+
+
+  const nextClickHandler = () =>{
+    if(person === 1){
+      setPerson(0);
+    }else{
+      setPerson((prevState) => {
+        return (prevState + 1);
+      });
+    }
+    const prevClickHandler = () =>{
+      if(person === 0){
+        setPerson(1);
+      }else{
+        setPerson((prevState) => {
+          return (prevState - 1);
+        });
+      }
+    }
+  }
+
+
+
   return (
-    <div className="App">
+    <div>
+      <img src={image} alt={data[person].name} />
+      <div>Sliderbuttons</div>
+      <p className={styles.quote}>" {data[person].quote} "</p>
+      <p className={styles.name}>{data[person].name}</p>
+      <p className={styles.title}>{data[person].title}</p>
     </div>
   );
 }
